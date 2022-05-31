0997eee574b1a5356df182421da7c2a1897f9b40
VidusionP <vidu_08@hotmail.com>
2022-05-31 18:40:39 -0400

    editted sound

--- components/PlaySound.js
+++ components/PlaySound.js
@@ -2,8 +2,10 @@ import React, {  useState, useEffect, useRef } from 'react';
 import Sound from 'react-sound';
 import {Howl, Howler} from 'howler';
 
-export default function PlaySound(props) {   
-    window.soundManager.setup({debugMode: false});
+export default function PlaySound(props) {
+    useEffect(() => {
+        window.soundManager.setup({debugMode: false});
+    })   
     return (
         <Sound
             url={'https://github.com/VidusionP/vidusion-puva/blob/main/jazz.mp3?raw=true'}
